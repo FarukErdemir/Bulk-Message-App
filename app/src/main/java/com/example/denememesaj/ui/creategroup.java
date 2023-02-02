0d3550b42c00439a7a2053f525da2e8920f80112
FarukErdemir <110052835+FarukErdemir@users.noreply.github.com>
2023-02-02 23:27:59 +0300

    Add files via upload

--- app/src/main/java/com/example/denememesaj/ui/creategroup.java
+++ app/src/main/java/com/example/denememesaj/ui/creategroup.java
@@ -1,66 +1,206 @@
 package com.example.denememesaj.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.denememesaj.ui.GroupModel;
 import com.example.denememesaj.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link creategroup#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.checkerframework.framework.qual.PurityUnqualified;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
 public class creategroup extends Fragment {
+    Uri filePath;
+    EditText groupNameEditText,groupDescriptionEditText;
+    ImageView groupImageView;
+    Button createGroupButton;
+    RecyclerView groupsRecyclerView;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FirebaseAuth mAuth;
+    FirebaseFirestore mStore;
+    FirebaseStorage mStorage;
 
-    public creategroup() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment creategroup.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static creategroup newInstance(String param1, String param2) {
-        creategroup fragment = new creategroup();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    ArrayList<GroupModel> groupModelArrayList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_creategroup, container, false);
+        View view =  inflater.inflate(R.layout.fragment_creategroup, container, false);
+
+        groupNameEditText = view.findViewById(R.id.name);
+        groupDescriptionEditText = view.findViewById(R.id.desc);
+        groupImageView = view.findViewById(R.id.icon);
+        createGroupButton = view.findViewById(R.id.creategrpbtn);
+        groupsRecyclerView = view.findViewById(R.id.groupsrv);
+
+        groupModelArrayList=new ArrayList<>();
+
+        mAuth = FirebaseAuth.getInstance();
+        mStore = FirebaseFirestore.getInstance();
+        mStorage = FirebaseStorage.getInstance();
+
+        //grup resmi seçme ve resim kaydetme fonksiyonu
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == getActivity().RESULT_OK) {
+                        Intent data = result.getData();
+                        filePath = data.getData();
+                        groupImageView.setImageURI(filePath);
+                    }
+                }
+        );
+        groupImageView.setOnClickListener(view1 -> {
+            Intent intent =new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            activityResultLauncher.launch(intent);
+        });
+
+        //butona basıldığında alanların kontrolü ve resim yükleme gerçekleştirme
+        createGroupButton.setOnClickListener(v ->{
+            String groupName = groupNameEditText.getText().toString();
+            String groupDescription =groupDescriptionEditText.getText().toString();
+
+            if (groupName.isEmpty()) {
+                Toast.makeText(getContext(), "Grup ismi gerekli", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (groupDescription.isEmpty()) {
+                Toast.makeText(getContext(), "Grup açıklaması gerekli", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (filePath!=null){
+                StorageReference storageReference=mStorage.getReference().child("resimler/"+UUID.randomUUID().toString());
+                storageReference.putFile(filePath).addOnSuccessListener(taskSnapshot -> {
+                    storageReference.getDownloadUrl().addOnSuccessListener(uri -> {
+                        String downloadUrl = uri.toString();
+                        Toast.makeText(getContext(), "Resim yüklendi", Toast.LENGTH_SHORT).show();
+                        CreateGroup(groupName, groupDescription, downloadUrl);
+                    });
+                });
+                return;
+            }else{
+                CreateGroup(groupName,groupDescription,null);
+            }
+
+
+        });
+        //grup verilerini çekiyoruz.
+        FetchGroups();
+
+        return view;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //firebasede grup oluşturmak için gerekli fonksiyonu yazıyoruz.
+    private void CreateGroup(String name, String description, String image) {
+        String userId = mAuth.getCurrentUser().getUid();
+
+        mStore.collection("/userdata/" + userId + "/" + "groups").add(new HashMap<String, Object>(){{
+            put("name", name);
+            put("description", description);
+            put("image", image);
+            put("numbers", new ArrayList<String>());
+        }}).addOnSuccessListener(documentReference -> {
+            Toast.makeText(getContext(), "Grpu başarıyla oluşturuldu", Toast.LENGTH_SHORT).show();
+
+            documentReference.get().addOnSuccessListener(documentSnapshot -> {
+                GroupModel groupModel = new GroupModel( name, description, image, (List<String>)documentSnapshot.get("numbers"), documentSnapshot.getId());
+                groupModelArrayList.add(groupModel);
+                groupsRecyclerView.getAdapter().notifyItemInserted(groupModelArrayList.size() - 1);
+            });
+        }).addOnFailureListener(e -> {
+            Toast.makeText(getContext(), "Grup oluşturulamadı", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    //firebaseden grupları çekmek için gerekli fonksiyonu yazıyoruz.(önce model)
+    private void FetchGroups(){
+        String userId = mAuth.getCurrentUser().getUid();
+
+        mStore.collection("/userdata/" + userId + "/" + "groups").get().addOnSuccessListener(queryDocumentSnapshots -> {
+            groupModelArrayList.clear();
+            for (DocumentSnapshot documentSnapshot : queryDocumentSnapshots.getDocuments()) {
+                GroupModel groupModel = new GroupModel(documentSnapshot.getString("name"), documentSnapshot.getString("description"), documentSnapshot.getString("image"), (List<String>)documentSnapshot.get("numbers"),documentSnapshot.getId());
+                groupModelArrayList.add(groupModel);
+            }
+
+            groupsRecyclerView.setAdapter(new GroupAdapter(groupModelArrayList));
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+            groupsRecyclerView.setLayoutManager(linearLayoutManager);
+
+        });
     }
 }
--- app/src/main/java/com/example/denememesaj/ui/createmessage.java
+++ app/src/main/java/com/example/denememesaj/ui/createmessage.java
@@ -3,64 +3,102 @@ package com.example.denememesaj.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.denememesaj.ui.MessageModel;
 import com.example.denememesaj.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link createmessage#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class createmessage extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    FirebaseAuth mAuth;
+    FirebaseFirestore mStore;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    EditText messageNameEditText, messageDescriptionEditText;
+    Button createMessageButton;
+    RecyclerView messagesRecyclerView;
 
-    public createmessage() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment createmessage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static createmessage newInstance(String param1, String param2) {
-        createmessage fragment = new createmessage();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    ArrayList<MessageModel> messageModelList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_createmessage, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_createmessage, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        mStore = FirebaseFirestore.getInstance();
+
+        messageModelList = new ArrayList<>();
+
+        messageNameEditText = view.findViewById(R.id.amemes);
+        messageDescriptionEditText = view.findViewById(R.id.mesmes);
+        createMessageButton = view.findViewById(R.id.crtmes);
+        messagesRecyclerView = view.findViewById(R.id.mesrv);
+
+        //butonumuza basıldığında alanların boş olup olmadoğını kontrol eden fonksiyonu yazıyoruz
+        createMessageButton.setOnClickListener(v ->{
+            String messageName = messageNameEditText.getText().toString();
+            String messageDescription = messageDescriptionEditText.getText().toString();
+
+            if (messageName.isEmpty()||messageDescription.isEmpty()){
+                Toast.makeText(getContext(), "Lütfen tüm alanları doldurun", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            CreateMessage(messageName, messageDescription);
+        });
+        //mesaj çekme
+        FetchMessage();
+        return view;
+    }
+    //mesajları firebase kaydeden fonksiyon toast mesajlı
+    private void CreateMessage(String messageName,String messageDescription){
+        String userId=mAuth.getCurrentUser().getUid();
+        mStore.collection("/userdata/" + userId + "/messages").add(new HashMap<String, String>(){{
+                    put("name", messageName);
+                    put("description", messageDescription);
+                }})
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getContext(), "Mesaj başarıyla oluşturuldu", Toast.LENGTH_SHORT).show();
+
+                    documentReference.get().addOnSuccessListener(documentSnapshot -> {
+                        MessageModel messageModel = new MessageModel(messageName, messageDescription, documentSnapshot.getId());
+                        messageModelList.add(messageModel);
+                        messagesRecyclerView.getAdapter().notifyItemInserted(messageModelList.size() - 1);
+                    });
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(getContext(), "Mesaj oluşturulurken bir hata oluştu", Toast.LENGTH_SHORT).show();
+                });
+    }
+    //mesajları firebaseden çekmek için gerekli fonksiyon
+    private void FetchMessage(){
+        String userId =mAuth.getCurrentUser().getUid();
+        mStore.collection("userdata/"+userId+"/messages").get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    messageModelList.clear();
+                    for (DocumentSnapshot documentSnapshot:queryDocumentSnapshots.getDocuments()){
+                        MessageModel messageModel=new MessageModel(documentSnapshot.getString("name"),documentSnapshot.getString("description"),documentSnapshot.getId());
+                        messageModelList.add(messageModel);
+                    }
+
+
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(getContext(), "Mesajlar alınırken bir hata oluştu", Toast.LENGTH_SHORT).show();
+                });
     }
 }
